12cec01b2f68fb9e314c1401d5c3ce935c4dc260
rj-rayat <rayatrj36@gmail.com>
2026-03-22 18:44:49 +0600

    Loop

---
+++ script.js
@@ -0,0 +1,78 @@
+// Loop  
+
+// For loop
+// While Loop
+// For of Loop
+
+// Market e kaj nai eitar
+
+// for (let i = 1; i <= 10000; i++ ){
+//     console.log(i)
+// }
+
+// for of
+
+// const list = ["Threee Piece", "Borka", "3 piece", "Lunngi", "Shirt", "Doller", "Chosma", "Punjabi"]
+
+// for (r of list){
+//     console.log(r)
+// }
+
+
+// While Loop
+
+// let number = 1;
+
+// while (number <= 10){
+//     console.log(number)
+//     number = number + 1
+// }
+
+
+// for (let i = 1; i <= 1000; i++){
+//     console.log("Sorry bro")
+// }
+
+
+// Even number 
+
+
+const mixNumber = [300, 661, 500, 441, 100, 11, 333, 67]
+// const even = []
+// const odd = []
+
+// for (m of mixNumber){
+//     // console.log(m)
+//     if (m % 2 == 1){
+//         console.log(m)
+//     }
+// }
+
+// for (let m of mixNumber){
+// //    if( m % 2== 0){
+// //     even.push(m)
+// //    }
+//    if( m % 2== 1){
+//     odd.push(m)
+//    }
+// }
+
+// console.log("Even number", even)
+
+// console.log("odd Number", odd)
+
+
+// let sum = 0;
+
+// for (let i of mixNumber){
+//     sum = sum + i
+// }
+
+// console.log(sum)
+
+
+let number  = 1
+
+for (let i = 1; i <= 10; i++){
+    console.log(`1 x ${}`)
+}
